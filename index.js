b4599aae57ef531c04dfa4d4460782e014cdb302
Rashonda Edwards <rashondaedwards@aol.com>
2023-04-02 21:11:34 -0700

    progress

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+
+
 // Refering to the HTML tag we created in index.html
 const canvas = document.querySelector('canvas')
 
@@ -6,121 +8,61 @@ const canvas = document.querySelector('canvas')
     ('2d') because this is a 2d game*/
 const canvascontext = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1500
+canvas.height = 700
 
-function canv(){
-    /* This makes the recatangle we created white */
+
+let x1_position = 10
+let y1_position = 10
+let p1_width = 40
+let p1_height = 40
+let x1_velocity_r = 0   //////////////////////////////////////////
+let x1_velocity_l = 0   //  created 2 variables for velocity in 
+let y1_velocity_d = 0   //  both directions because it makes the 
+let y1_velocity_u = 0   //  transition of botton pressing smoother
+                        //////////////////////////////////////////
+let canvas_x_pos = 0
+let canvas_y_pos = 0
+let canvas_x_vel_r = 0
+let canvas_x_vel_l = 0
+let canvas_y_vel_d = 0
+let canvas_y_vel_u = 0
+
+function update() {
+    canvas_x_pos += canvas_x_vel_r
+    canvas_x_pos += canvas_x_vel_l
+    canvas_y_pos += canvas_y_vel_d
+    canvas_y_pos += canvas_y_vel_u
+    canvascontext.clearRect(canvas_x_pos, canvas_y_pos,canvas.width,canvas.height)
     canvascontext.fillStyle = 'white'
-
-    /* creates a rectangle in the canvas: 
-        c.fillRect(x-coordinate, y-coordinate, width, height); all in pixels */
-    canvascontext.fillRect(0, 0, canvas.width, canvas.height)
-}
-
-
-function play1(){
-    // this will be player
+    canvascontext.fillRect(canvas_x_pos, canvas_y_pos, canvas.width, canvas.height)
+    x1_position += x1_velocity_r
+    x1_position += x1_velocity_l
+    y1_position += y1_velocity_d
+    y1_position += y1_velocity_u
     canvascontext.fillStyle = 'blue'
-    let x1_position = 10
-    let y1_position = 100
-    let p1_width = 50
-    let p1_height = 50
     canvascontext.fillRect(x1_position, y1_position, p1_width, p1_height)
+    requestAnimationFrame(update)
+
+    canvascontext.fillStyle = 'green'
+    canvascontext.fillRect(200, 200, 10, 200)
+    canvascontext.fillStyle = 'green'
+    canvascontext.fillRect(500, 200, 10, 200)
 }
 
+update()
 
-function cpu(){
-    // this will be the cpu
-    canvascontext.fillStyle = 'red'
-    let x2_position = 10
-    let y2_position = 400
-    let p2_width = 50
-    let p2_height = 50
-    canvascontext.fillRect(x2_position, y2_position, p2_width, p2_height)
-}
-
-canv()
-play1()
-cpu()
-
-// Long wall in the middle of canvas!
-canvascontext.fillStyle = 'green'
-canvascontext.fillRect(0, 270, 20000, 10)
-
-// bottom wall
-canvascontext.fillStyle = 'green'
-canvascontext.fillRect(1000, 120, 10, 160)
-
-// top wall
-canvascontext.fillStyle = 'green'
-canvascontext.fillRect(1500, 0, 10, 150)
-
-// middle wall (top)
-canvascontext.fillStyle = 'green'
-canvascontext.fillRect(2000, 0, 10, 80)
-// middle wall (bottom)
-canvascontext.fillStyle = 'green'
-canvascontext.fillRect(2000, 200, 10, 80)
-
-
-
-
-/* 
-
-// Walls to avoid
-class Wall{
-    constructor(x_position, y_position, width, height){
-        this.x_position = x_position
-        this.y_position = y_position
-        this.width = width
-        this.height = height
-    }
-    color(){
-        canvascontext.fillStyle = 'green'
-    }
-    position(){
-        canvascontext.fillRect(x_position, y_position, width, height)
-    }
-}
-
-const wall1 = new Wall(0, 250, 2000, 10)
-
-
-*/
-
-
-
-
-function animate(){}
-
-
-window.addEventListener('keydown', (event)=>{
-    switch (event.key) {
-        case 'ArrowRight':
-            player.velocity.x = 5
-            break;
-        case 'ArrowLeft':
-            player.velocity.x = -5
-            break;
-        case ' ':
-            /*Pressing the spacebar will make the player jump*/
-            player.velocity.y = -15
-            break; 
-        default:
-            break;
-    }
+addEventListener('keydown', (event)=>{
+    if (event.code == 'ArrowRight') x1_velocity_r = 5, canvas_x_vel_r = 5
+    if (event.code == 'ArrowLeft') x1_velocity_l = -5, canvas_x_vel_l = -5
+    if (event.code == 'ArrowDown') y1_velocity_d = 5, canvas_y_vel_d = 5
+    if (event.code == 'ArrowUp') y1_velocity_u = -5, canvas_y_vel_u = -5
 })
 
-window.addEventListener('keyup', (event)=>{
-    switch (event.key) {
-        case 'ArrowRight':
-            player.velocity.x = 0
-            break;
-        case 'ArrowLeft':
-            player.velocity.x = 0
-            break;
-        default:
-            break;
-    }
+addEventListener('keyup', (event)=>{
+    if (event.code == 'ArrowRight') x1_velocity_r = 0, canvas_x_vel_r = 0
+    if (event.code == 'ArrowLeft') x1_velocity_l = 0, canvas_x_vel_l = 0
+    if (event.code == 'ArrowDown') y1_velocity_d = 0, canvas_y_vel_d = 0
+    if (event.code == 'ArrowUp') y1_velocity_u = 0, canvas_y_vel_u = 0
 })
+
